d084206d73e44624c3c8555db8e4ca04f51dc54f
therealjacobbrauer <42282968+therealjacobbrauer@users.noreply.github.com>
2020-02-13 23:54:29 -0800

    Add files via upload

---
+++ tauestv0-0.py
@@ -0,0 +1,12 @@
+
+
+import numpy as np
+
+# f(x,y,z,orientation(pitch),yaw, ) for each particle
+# number rows- 1 is number of particles
+# mouth is origin
+# array for spring consts between each particle. 
+# 2 arms, 2 legs are springs and torso is stiff. 
+
+#tau code
+
